24eaaac6fcb4d903b45ce8ef61f68c37b879ab58
Dessertion <73731354pi@gmail.com>
2018-10-15 23:49:31 -0400

    made some small basic changes

--- src/com/Dessertion/src/Core.java
+++ src/com/Dessertion/src/Core.java
@@ -1,13 +1,43 @@
 package com.Dessertion.src;
 
-public class Core {
+import javax.swing.JFrame;
 
-    public Core() {
+public class Core extends JFrame implements Runnable{
+	private Thread thread;
+	private static final int WIDTH = 800, HEIGHT = 600;
+	public boolean isRunning;
 	
+    public Core() {
+    	thread = new Thread(this);
+    	setResizable(false);
+    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	setSize(WIDTH,HEIGHT);
+    	setTitle("Test Game");
+    	setVisible(true);
+    }
+    
+    private synchronized void start() {
+    	
+    }
+    
+    public synchronized void stop() {
+    	
+    }
+    
+    public void run() {
+    	
+    }
+    
+    public int getFrameWidth() {
+    	return WIDTH;
+    }
+    
+    public int getFrameHeight() {
+    	return HEIGHT;
     }
     
     public static void main(String[] args) {
-	
+    	
     }
 
 }
